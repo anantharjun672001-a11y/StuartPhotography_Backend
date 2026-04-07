fc9678193a82f448a1fb1c12166389a94be3034c
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-07 10:49:36 +0530

    backend ready

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT} `);
 });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running  ");
+});
